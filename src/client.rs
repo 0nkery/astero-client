aad4ff31a9eed17b8008260b37b23dabfcfb431e
Dmitry Shlagoff <richyannet@yandex.ru>
2017-12-07 15:18:28 +0700

    More clear names for channel parts + actually sending messages

--- src/client.rs
+++ src/client.rs
@@ -102,8 +102,8 @@ pub struct Client {
 
 impl Client {
     pub fn start() -> Self {
-        let (from_client_tx, from_client_rx) = std_mpsc::channel();
-        let (to_client_tx, to_client_rx) = futures_mpsc::unbounded();
+        let (to_main_thread, from_client) = std_mpsc::channel();
+        let (to_client, from_main_thread) = futures_mpsc::unbounded();
 
         let thread_handle = thread::spawn(move || {
             let mut reactor = Core::new().expect("Failed to init reactor");
@@ -114,20 +114,18 @@ impl Client {
                 UdpSocket::bind(&client_address, &handle)
                     .expect("Failed to create socket");
 
-            let (mut outgoing, ingoing) =
+            let (outgoing, ingoing) =
                 socket.framed(ClientCodec::new()).split();
-            let outgoing_ref = &mut outgoing;
 
             let receiver = ingoing.for_each(|msg| {
-                from_client_tx.send(msg).expect("Failed to drop message to the main thread");
+                to_main_thread.send(msg).expect("Failed to drop message to the main thread");
                 Ok(())
             });
 
-            let sender =
-                to_client_rx.for_each(|msg| {
-                    outgoing_ref.send(msg).wait().expect("Failed to send UDP packet");
-                    Ok(())
-                }).map_err(|_err| io::ErrorKind::Other.into());
+            let from_main_thread = from_main_thread
+                .map_err(|_| -> io::Error { io::ErrorKind::Other.into() });
+
+            let sender = outgoing.send_all(from_main_thread);
 
             let client = sender.join(receiver);
             reactor.run(client).expect("Failed to start client");
@@ -135,8 +133,8 @@ impl Client {
 
         Client {
             thread_handle,
-            to: to_client_tx,
-            from: from_client_rx
+            to: to_client,
+            from: from_client
         }
     }
 
