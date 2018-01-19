ebae576f7024bd899d4be27d62937bf4150b6b84
Dmitry Shlagoff <richyannet@yandex.ru>
2018-01-19 14:11:07 +0700

    Refactored access to sprites

--- src/resources/assets.rs
+++ src/resources/assets.rs
@@ -1,44 +1,44 @@
+use std::collections::BTreeMap;
+
 use ggez::{
     self,
     graphics,
 };
 
 
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+pub enum SpriteKind {
+    Player = 0,
+    Asteroid = 1,
+    Shot = 2,
+}
+
+
 pub struct Assets {
-    player_image: graphics::Image,
-    shot_image: graphics::Image,
-    rock_image: graphics::Image,
+    sprites: BTreeMap<SpriteKind, graphics::Image>,
     font: graphics::Font,
     small_font: graphics::Font,
 }
 
 impl Assets {
     pub fn new(ctx: &mut ggez::Context) -> ggez::GameResult<Self> {
-        let player_image = graphics::Image::new(ctx, "/player.png")?;
-        let shot_image = graphics::Image::new(ctx, "/shot.png")?;
-        let rock_image = graphics::Image::new(ctx, "/rock.png")?;
+        let mut sprites = BTreeMap::new();
+
+        sprites.insert(SpriteKind::Player, graphics::Image::new(ctx, "/player.png")?);
+        sprites.insert(SpriteKind::Asteroid, graphics::Image::new(ctx, "/asteroid.png")?);
+        sprites.insert(SpriteKind::Shot, graphics::Image::new(ctx, "/shot.png")?);
 
         let font = graphics::Font::new(ctx, "/DejaVuSerif.ttf", 18)?;
         let small_font = graphics::Font::new(ctx, "/DejaVuSerif.ttf", 12)?;
 
         Ok(Self {
-            player_image,
-            shot_image,
-            rock_image,
+            sprites,
             font,
             small_font,
         })
     }
 
-    fn shot_image(&self) -> &graphics::Image {
-        &self.shot_image
-    }
-
-    fn asteroid_image(&self) -> &graphics::Image {
-        &self.rock_image
-    }
-
-    fn player_image(&self) -> &graphics::Image {
-        &self.player_image
+    pub fn get_sprite(&self, kind: &SpriteKind) -> &graphics::Image {
+        self.sprites.get(kind).expect("Sprite not found")
     }
 }
--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -4,7 +4,10 @@ mod input;
 mod server_clock;
 
 
-pub use self::assets::Assets;
+pub use self::assets::{
+    Assets,
+    SpriteKind,
+};
 pub use self::client::Client;
 pub use self::input::Input;
 pub use self::server_clock::ServerClock;
