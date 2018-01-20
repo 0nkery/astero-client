0a823430cbea808d98f07052b51cf68e48368e77
Dmitry Shlagoff <wessmoke@gmail.com>
2018-01-20 20:33:34 +0700

    More useful 'Life' component definition

--- src/components/life.rs
+++ src/components/life.rs
@@ -1,2 +1,18 @@
 #[derive(Component, Debug)]
-pub struct Life(f32);
+pub struct Life {
+    cur: f32,
+    max: f32,
+}
+
+impl Life {
+    pub fn new(max: f32) -> Self {
+        Self {
+            cur: max,
+            max,
+        }
+    }
+
+    pub fn fraction(&self) -> f32 {
+        self.cur / self.max
+    }
+}
